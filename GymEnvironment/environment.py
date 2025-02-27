48c82887a9982e14ae04479b7b9f9f53f59983a2
trank4 <khoa.tran.uc@gmail.com>
2025-02-27 01:40:50 -0500

    Khoa: update the environment, add script to do benchmark evaluation for random policy

--- GymEnvironment/environment.py
+++ GymEnvironment/environment.py
@@ -6,33 +6,44 @@ from pde.AdvectionEquation import  *
 
 class SensorOptimalPlacement(gym.Env):
     metadata = {"render_modes": [None]} #todo: enable render later
-    def __init__(self, width, length, n_sensor,seed):
+    def __init__(self, width, length, n_sensor, max_horizon, N_max, seed):
         super().__init__()
         self.width = width # Must be consistent with pde_config
         self.length = length # Must be consistent with pde_config
         self.grid_size = width * length
         self.seed = seed
+        np.random.seed(seed)
         self.n_sensor = n_sensor
+        self.max_horizon = max_horizon #max horizon by which an epsisode will be terminated
+        self.N_max = N_max #max iteration of nonincreasing reward that an episode will be terminated
         #define observation space
         #observation space is a grid with 1 and 0 depicting positions of sensors
         self.observation_space = spaces.MultiBinary((self.width, self.length), seed = self.seed)
         # define action space
         # fof action, at each step, we choose a sensor and move to a new available positions => num_actions  = n_sensor * (width * length - n_sensor)
         # an action is a discrete integer
-        self.action_space = spaces.Discrete(self.n_sensor * (self.grid_size - self.n_sensor))
+        self.action_space = spaces.Discrete(self.n_sensor * (self.grid_size - self.n_sensor), seed= self.seed)
 
         #initiate the pde dynamics
         self.pde_config = Adv2dModelConfig()
+        assert self.pde_config.nx == width, "x-dimension mismatch"
+        assert self.pde_config.ny == length, "y-dimension mismatch"
+
         self.pde_solver= Advection2D(config=self.pde_config)
 
         #internal tracking
         self.state = None  # Will be a (width x length) array of 0/1
         self.sensor_positions = None  # List of (row, col) for each sensor
         self.pde_field = None #PDE solution field, of shape (n_step, width, length) # n_step defined in pde_config
+        self.max_reward = -np.inf
+        self.N = 0 #number of steps since reward last improved
+        self.optimal_state = None
+        self.t = 0 #time step in current episode
 
-
-    def reset(self):
+    def reset(self, seed = None, options = None):
         """initiate new episode by randomly placing n_sensor sensors on the grid"""
+
+        super().reset(seed = seed)
         # Create an empty grid
         self.state = np.zeros((self.width, self.length), dtype=int)
         # Choose n_sensor unique cells to place sensors
@@ -45,9 +56,15 @@ class SensorOptimalPlacement(gym.Env):
         for (r, c) in self.sensor_positions:
             self.state[r, c] = 1
 
-        #set initial condition for pde solver
+        #simulate the dynamics (only once at the beginning of the new episode)
         pde_initial_cond =  self.pde_solver.initial_condition()
         self.pde_field = self.pde_solver.step(pde_initial_cond)
+
+        #reset tracking state
+        self.N = 0
+        self.t = 0
+        self.max_reward = -np.inf
+        self.optimal_state = None
 
         return self.state
 
@@ -75,14 +92,25 @@ class SensorOptimalPlacement(gym.Env):
         #compute rewards
         reward = self._compute_reward(self.pde_field, self.state, self.sensor_positions)
 
-        #step pde_solver for next step, use the last frame of current pde_field as current state
-        current_state = self.pde_field[-1]
-        self.pde_field = self.pde_solver.step(current_state)
+        #update max reward
+        if reward > self.max_reward:
+            self.max_reward = reward
+            self.optimal_state = self.state.copy()
+            self.N = 0
+        else:
+            self.N += 1
 
-        #define terminal condition (not sure?)
-        done = False
-        info = {}
-        return self.state, reward, done, info
+        #define terminal condition
+        self.t += 1
+        if (self.N > self.N_max) or (self.t > self.max_horizon):
+            done = True
+        else:
+            done = False
+
+        info = {"optimal_state": self.optimal_state,
+                "max_reward": self.max_reward}
+
+        return self.state, reward, done, False, info
 
     def _compute_reward(self, pde_field, grid_state, sensor_positions):
         #run KL decomposition, then calculate criteria score
